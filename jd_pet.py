348128232e46dd6e7dee533bf3a67cbf543ded97
hathwkfg <49889707+hathwkfg@users.noreply.github.com>
2020-11-07 15:27:24 +0800

    Update jd_pet.py

--- jd_pet.py
+++ jd_pet.py
@@ -16,7 +16,7 @@ import notification
 feedTimesLimit = 12
 retainFoodAmountLimit = 40  # 完成10次喂养任务的基础上,希望food始终高于此数;优先级高于feedTimesLimit
 shareCodes = [
-    "MTAxODc2NTEzMTAwMDAwMDAwOTYwNDkzMQ==", "MTAxODcxOTI2NTAwMDAwMDAwMTY0NTc4OQ==", "MTAxODc2NTEzMDAwMDAwMDAyNjYzMDQ3MQ=="
+    
 ]  # 自己不能助力自己,填写他人的助力码
 
 
